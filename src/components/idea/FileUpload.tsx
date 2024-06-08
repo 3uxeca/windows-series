8d92408932f60ee224d1aa9e554e2f8aef796596
3uxeca <sajihye4@gmail.com>
2024-06-08 17:25:30 +0900

    - CustomSelect 추가 - 테이블 추가 - 상품가격결정/매출계획수립 페이지 작업

--- src/components/idea/FileUpload.tsx
+++ src/components/idea/FileUpload.tsx
@@ -1,6 +1,6 @@
-'use client'
-import React, { useEffect, useState } from 'react'
-import styled from '@/components/idea/Idea.module.scss';
+"use client";
+import React, { useEffect, useState } from "react";
+import styled from "@/components/idea/Idea.module.scss";
 
 interface UploadDataProps {
   uploadData: File[];
@@ -12,23 +12,27 @@ interface UploadDataProps {
   id: string;
 }
 
-const FileUpload: React.FC<UploadDataProps> = ({ 
-  uploadData, 
-  setUploadData, 
-  // setReadyUpload, 
-  extList, 
+const FileUpload: React.FC<UploadDataProps> = ({
+  uploadData,
+  setUploadData,
+  // setReadyUpload,
+  extList,
   limitCnt,
   type,
-  id
+  id,
 }) => {
   const [files, setFiles] = useState<File[]>([]);
   const [deleteFiles, setDeleteFiles] = useState<File[]>([]);
   const [totalFiles, setTotalFiles] = useState<File[]>([]);
   const [filesSrc, setFilesSrc] = useState<string[]>([]);
-  const [filesNm, setFilesNm] = useState<string[]>([]);  
-  const acceptValue = extList ? extList.map(ext => `${type}/${ext}`).join(", ") : '';
+  const [filesNm, setFilesNm] = useState<string[]>([]);
+  const acceptValue = extList
+    ? extList.map((ext) => `${type}/${ext}`).join(", ")
+    : "";
 
-  const handleChangeFile = (e: React.ChangeEvent<HTMLInputElement> | React.DragEvent<HTMLDivElement>) => {
+  const handleChangeFile = (
+    e: React.ChangeEvent<HTMLInputElement> | React.DragEvent<HTMLDivElement>
+  ) => {
     let newFileLength: number;
     let selectedFiles: FileList;
     let number = totalFiles.length;
@@ -65,7 +69,7 @@ const FileUpload: React.FC<UploadDataProps> = ({
         if (fileSize > 1048576) {
           const kilobyte = fileSize / 1024;
           const megabyte = (kilobyte / 1024).toFixed(2);
-          if (parseFloat(megabyte) > 20.00) {
+          if (parseFloat(megabyte) > 20.0) {
             alert(`파일 용량을 확인해주세요.\n최대 파일 크기 : 1GB`);
             continue;
           } else {
@@ -80,88 +84,103 @@ const FileUpload: React.FC<UploadDataProps> = ({
         const reader = new FileReader();
         reader.readAsDataURL(selectedFiles[i]);
         reader.onload = () => {
-          setFilesSrc(filesSrc => [...filesSrc, reader.result as string]);
-        }
+          setFilesSrc((filesSrc) => [...filesSrc, reader.result as string]);
+        };
 
         const newFile = selectedFiles[i];
         // newFile["no"] = number + 1 + i;
 
-        setFilesNm(filesNm => [...filesNm, name]);
-        setFiles(files => [...files, newFile]);
-        setTotalFiles(totalFiles => [...totalFiles, newFile]);
-        setUploadData(uploadData => [...uploadData, newFile]);
+        setFilesNm((filesNm) => [...filesNm, name]);
+        setFiles((files) => [...files, newFile]);
+        setTotalFiles((totalFiles) => [...totalFiles, newFile]);
+        setUploadData((uploadData) => [...uploadData, newFile]);
         // setReadyUpload(true);
       }
     }
-  }  
+  };
 
   const onRemove = (index: number) => {
     const target = totalFiles[index];
-    setDeleteFiles(deleteFiles => [...deleteFiles, target]);
+    setDeleteFiles((deleteFiles) => [...deleteFiles, target]);
     setTotalFiles(totalFiles.filter((file, idx) => idx !== index));
     setUploadData(uploadData.filter((file, idx) => idx !== index));
     // setFilesNm(filesNm.filter(nm => nm !== target.name));
     const cur = [...filesSrc];
     cur.splice(index, 1);
     setFilesSrc(cur);
-  }
+  };
 
   // useEffect(() => {
   //   if (totalFiles.length < 1) {
   //     setReadyUpload(false);
   //   }
-  // }, [totalFiles]);  
+  // }, [totalFiles]);
 
   const onDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     e.stopPropagation();
-  }
+  };
 
   const onDragOver = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     e.stopPropagation();
-  }
+  };
 
   const onFileDrop = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     e.stopPropagation();
     handleChangeFile(e);
-  }
+  };
 
   const onDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     e.stopPropagation();
-  }  
-  
+  };
+
   return (
     <>
-      {uploadData?.length === 0 ?
+      {uploadData?.length === 0 ? (
         <div className={`${styled.uploadWrap}`}>
           <input
             id={id}
             type="file"
             accept={acceptValue}
-            onChange={e => handleChangeFile(e)}
+            onChange={(e) => handleChangeFile(e)}
             multiple
           />
           <div className={styled.uploadBox}>
-            <div className={styled.dropZone} onDrop={e => onFileDrop(e)} onDragEnter={e => onDragEnter(e)} onDragOver={e => onDragOver(e)} onDragLeave={e => onDragLeave(e)}>
-              <div className={styled.uploadTxt}>파일을 폴더에서 찾거나, 드래그 앤 드롭(Drag and Drop)으로 파일을 업로드 해주세요.</div>
+            <div
+              className={styled.dropZone}
+              onDrop={(e) => onFileDrop(e)}
+              onDragEnter={(e) => onDragEnter(e)}
+              onDragOver={(e) => onDragOver(e)}
+              onDragLeave={(e) => onDragLeave(e)}
+            >
+              <div className={styled.uploadTxt}>
+                파일을 폴더에서 찾거나, 드래그 앤 드롭(Drag and Drop)으로 파일을
+                업로드 해주세요.
+              </div>
               <label htmlFor={id} className={styled.uploadIcon}></label>
             </div>
           </div>
         </div>
-        :
+      ) : (
         <div className={`${styled.uploadWrap}`}>
           <input
             id={id}
             type="file"
             accept={acceptValue}
-            onChange={e => handleChangeFile(e)}
+            onChange={(e) => handleChangeFile(e)}
             multiple
           />
           <div className={`${styled.uploadBox} ${styled.uploaded}`}>
-            <div className={styled.dropZone} onDrop={e => onFileDrop(e)} onDragEnter={e => onDragEnter(e)} onDragOver={e => onDragOver(e)} onDragLeave={e => onDragLeave(e)}>
+            <div
+              className={styled.dropZone}
+              onDrop={(e) => onFileDrop(e)}
+              onDragEnter={(e) => onDragEnter(e)}
+              onDragOver={(e) => onDragOver(e)}
+              onDragLeave={(e) => onDragLeave(e)}
+            >
               <div className={styled.uploadList}>
                 {uploadData?.length > 0 &&
                   uploadData.map((it, idx) => {
@@ -169,22 +188,26 @@ const FileUpload: React.FC<UploadDataProps> = ({
                       <div
                         key={`${id}_${idx}`}
                         className={styled.filename}
-                        // onClick={() => onRemove(idx)}
+                        onClick={() => onRemove(idx)}
                       >
                         {it.name}
-                        <div className={styled.delete} onClick={() => onRemove(idx)}>X</div>
+                        <div
+                          className={styled.delete}
+                          // onClick={() => onRemove(idx)}
+                        >
+                          x
+                        </div>
                         {/* <img src={it} alt={it} /> */}
                       </div>
-                    )
-                  })
-                }
+                    );
+                  })}
               </div>
             </div>
           </div>
-        </div>             
-      }
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default FileUpload
+export default FileUpload;
